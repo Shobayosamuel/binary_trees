07d45c2b1232ace11d6a008d9db7f9670478dc0c
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-02 01:01:34 +0100

    corrected betty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,12 +3,13 @@
 /**
  * binary_tree_depth - measures the depth of a node
  * @tree: pinter to the node
- * Return - depth of the node
+ * Return: depth of the node
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t num = 0;
 	const binary_tree_t *temp = tree;
+
 	if (tree == NULL)
 		return (0);
 	while (temp->parent)
