c34bfde00686000c5e8ab819778cd799afc11f35
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-02 01:23:42 +0100

    Size of the node

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measure the size of the node
+ * @tree: pointer to the node
+ * Return: Size of the node
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+	size += 1;
+	size +=	binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	return (size);
+}
