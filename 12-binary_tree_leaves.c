5347f1693e465cafe196e710e506f7db13c9d1e9
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-03-02 10:33:47 +0100

    fixed bug

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,13 +7,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t num = 0;
 
-	if (tree)
-	{
-		num = 1 + binary_tree_leaves(tree->left);
-		num = 1 +  binary_tree_leaves(tree->left);
+	if (tree == NULL)
+		return (0);
 
-	}
-	return (num);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
